eae4380c2504eb4fcece727989ff7481cc3b41dd
XiangSi <3059181470@qq.com>
2022-09-03 15:45:29 +0800

    1

---
+++ Mall/src/main/java/com/example/mall/Controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.mall.Controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+
+}
